a6d114cc8508fd168c645ce9f0b3696efd8a313a
EdanToledo <edan.toledo@live.com>
2023-10-10 15:42:00 +0000

    work-in-progress

--- maml.py
+++ maml.py
@@ -20,20 +20,21 @@ class TimeStep:
     behaviour_value: chex.Array
 
 def get_network_fn(num_outputs: int):
+    """Returns a network function that can be used to compute the logits and value of the agent."""
     
     def network_fn(obs: chex.Array) -> chex.Array:
         x = hk.Sequential([
-            hk.Linear(128),
-            jax.nn.relu])(obs)
+            hk.Linear(256),
+            jax.nn.leaky_relu])(obs)
 
         logits = hk.Sequential([
-            hk.Linear(128), 
-            jax.nn.relu,
+            hk.Linear(256), 
+            jax.nn.leaky_relu,
             hk.Linear(num_outputs)])(x)
         
         value = hk.Sequential([
-            hk.Linear(128), 
-            jax.nn.relu,
+            hk.Linear(256), 
+            jax.nn.leaky_relu,
             hk.Linear(1)])(x)
 
         return logits, value
@@ -45,13 +46,13 @@ def mutate_env_params(env_params : EnvParams, rng : chex.PRNGKey):
     keys = jax.random.split(rng, 8)
     noise_scale = 0.0
     return env_params.replace(  gravity = 9.8 + jax.random.uniform(keys[0], minval=-noise_scale, maxval=noise_scale),
-                                masscart = 1.0 + jax.random.uniform(keys[0], minval=-noise_scale, maxval=noise_scale),
-                                masspole = 0.1 + jax.random.uniform(keys[0], minval=-noise_scale, maxval=noise_scale),
-                                total_mass = 1.0 + 0.1 + jax.random.uniform(keys[0], minval=-noise_scale, maxval=noise_scale),  # (masscart + masspole)
-                                length = 0.5 + jax.random.uniform(keys[0], minval=-noise_scale, maxval=noise_scale),
-                                polemass_length = 0.05 + jax.random.uniform(keys[0], minval=-noise_scale, maxval=noise_scale),  # (masspole * length)
-                                force_mag = 10.0 + jax.random.uniform(keys[0], minval=-noise_scale, maxval=noise_scale),
-                                tau = 0.02 + jax.random.uniform(keys[0], minval=-noise_scale, maxval=noise_scale))
+                                masscart = 1.0 + jax.random.uniform(keys[1], minval=-noise_scale, maxval=noise_scale),
+                                masspole = 0.1 + jax.random.uniform(keys[2], minval=-noise_scale, maxval=noise_scale),
+                                total_mass = 1.0 + 0.1 + jax.random.uniform(keys[3], minval=-noise_scale, maxval=noise_scale),  # (masscart + masspole)
+                                length = 0.5 + jax.random.uniform(keys[4], minval=-noise_scale, maxval=noise_scale),
+                                polemass_length = 0.05 + jax.random.uniform(keys[5], minval=-noise_scale, maxval=noise_scale),  # (masspole * length)
+                                force_mag = 10.0 + jax.random.uniform(keys[6], minval=-noise_scale, maxval=noise_scale),
+                                tau = 0.02 + jax.random.uniform(keys[7], minval=-noise_scale, maxval=noise_scale))
 
 def sample_tasks(base_env_params : EnvParams, rng_key, num_tasks):
     """Samples multiple tasks from the base environment params."""
@@ -100,7 +101,7 @@ def get_meta_learner_fn(
         batch = jax.tree_map(lambda x: jnp.squeeze(x, 0), batch)
         logits_t, v_t = network_fn.apply(params, batch.observation)  
         a_log_prob_t = distrax.Categorical(logits_t).log_prob(batch.action.astype(jnp.int32)) 
-        adv_t = rlax.truncated_generalized_advantage_estimation(batch.reward[:-1], batch.discount[1:] * agent_discount, gae_lambda, batch.behaviour_value, True) 
+        adv_t = rlax.truncated_generalized_advantage_estimation(batch.reward[:-1], batch.discount[:-1] * agent_discount, gae_lambda, batch.behaviour_value, True) 
         rhos = jnp.exp(a_log_prob_t[:-1] - batch.behaviour_action_log_prob[:-1])
         pg_loss = rlax.clipped_surrogate_pg_loss(rhos, adv_t, clip_epsilon)
         entropy_loss = distrax.Categorical(logits_t).entropy()[:-1]
@@ -117,12 +118,14 @@ def get_meta_learner_fn(
         rollout = jax.vmap(sample_trajectory, in_axes=(None, 0, None))(inner_params, trajectory_keys, env_params)
         batch_loss = jax.vmap(inner_policy_loss_fn, in_axes=(None, 0))
         loss = lambda params, batch: jnp.mean(batch_loss(params, batch))
-        grads = jax.grad(  # compute gradient on a single trajectory.
+        inner_loss, grads = jax.value_and_grad(  # compute gradient on a single trajectory.
             loss)(inner_params, rollout)
+        grads = jax.lax.pmean(grads, axis_name="tasks")  # average gradients across tasks.
+        grads = jax.lax.pmean(grads, axis_name="devices")  # average gradients across devices.
         updates, new_opt_state = opt_update_fn.update(grads, inner_opt_state)  # transform grads.
         new_params = optax.apply_updates(inner_params, updates)  # update parameters.
        
-        return new_params, new_opt_state
+        return new_params, new_opt_state, inner_loss
 
 
     def inner_loop_fn(meta_params : hk.Params, meta_opt_state : optax.OptState, rng_key : chex.PRNGKey, env_params):
@@ -130,47 +133,54 @@ def get_meta_learner_fn(
         
         rng_key, burn_in_rng, update_rng = jax.random.split(rng_key, 3)
 
-        def rollout_fn_step(carry, rng_key):
-            inner_params, inner_opt_state, env_params = carry
-            inner_params, inner_opt_state = inner_policy_update_fn(inner_params, inner_opt_state, rng_key, env_params)
-            return (inner_params, inner_opt_state, env_params), None
+        # def rollout_fn_step(carry, rng_key):
+        #     inner_params, inner_opt_state, env_params = carry
+        #     inner_params, inner_opt_state, inner_loss = inner_policy_update_fn(inner_params, inner_opt_state, rng_key, env_params)
+        #     return (inner_params, inner_opt_state, env_params), None
 
-        (updated_inner_params, _, env_params), _ = jax.lax.scan(rollout_fn_step, (meta_params, meta_opt_state, env_params), jax.random.split(burn_in_rng, inner_loop_burn_in))
+        # (updated_inner_params, _, env_params), _ = jax.lax.scan(rollout_fn_step, (meta_params, meta_opt_state, env_params), jax.random.split(burn_in_rng, inner_loop_burn_in))
             
         trajectory_keys = jax.random.split(update_rng, num_trajectories)
-        rollout = jax.vmap(sample_trajectory, in_axes=(None, 0, None))(updated_inner_params, trajectory_keys, env_params)
+        rollout = jax.vmap(sample_trajectory, in_axes=(None, 0, None))(meta_params, trajectory_keys, env_params)
         batch_loss = jax.vmap(inner_policy_loss_fn, in_axes=(None, 0))
-        loss = jnp.mean(batch_loss(updated_inner_params, rollout))
+        loss = jnp.mean(batch_loss(meta_params, rollout))
 
         return loss
     
-    def meta_loss(meta_params, meta_train_state, inner_loop_key, task_env_params):
+    def meta_loss(meta_params, meta_opt_state, inner_loop_key, task_env_param):
             """Computes the meta-loss."""
-            outer_loss = jax.vmap(inner_loop_fn, in_axes=(None, None, 0, 0))(meta_params, meta_train_state, jax.random.split(inner_loop_key, num_tasks), task_env_params)
+            outer_loss = inner_loop_fn(meta_params, meta_opt_state, inner_loop_key, task_env_param)
             outer_loss = jnp.mean(outer_loss)
             return outer_loss
         
-    def outer_fn(meta_params : hk.Params, meta_opt_state : optax.OptState, rng_key : chex.PRNGKey):
+    def outer_fn(meta_params : hk.Params, meta_opt_state : optax.OptState, rng_key : chex.PRNGKey, task_env_param : EnvParams):
         """Performs a single update of the meta-parameters."""
         rng_key, mutation_key, inner_loop_key = jax.random.split(rng_key, 3)
-        task_env_params = sample_tasks(init_env_params, mutation_key, num_tasks)
-        
-        meta_loss_value, grads = jax.value_and_grad(meta_loss)(meta_params, meta_opt_state, inner_loop_key, task_env_params)
-       
-        updates, new_meta_opt_state = opt_update_fn.update(grads, meta_opt_state)  # transform grads.
-        new_meta_params = optax.apply_updates(meta_params, updates)  # update parameters.
+        # We get the loss and gradients of the meta objective.
+        # meta_loss_value, grads = jax.value_and_grad(meta_loss)(meta_params, meta_opt_state, inner_loop_key, task_env_param)
+        # grads = jax.lax.pmean(grads, axis_name="tasks")  # average gradients across tasks.
+        # grads = jax.lax.pmean(grads, axis_name="devices")  # average gradients across devices.
+        # updates, new_meta_opt_state = opt_update_fn.update(grads, meta_opt_state)  # transform grads.
+        # new_meta_params = optax.apply_updates(meta_params, updates)  # update parameters.
 
+        new_meta_params, new_meta_opt_state, meta_loss_value = inner_policy_update_fn(meta_params, meta_opt_state, inner_loop_key, task_env_param)
+        
         return new_meta_params, new_meta_opt_state, meta_loss_value
         
     def meta_learner_fn(init_meta_params, init_meta_opt_state, rng_key : chex.PRNGKey):
         """Performs multiple updates of the meta-agent."""
 
-        def iterate_fn(meta_train_state, rng_key):  # repeat many times to avoid going back to Python.
-            meta_params, meta_opt_state = meta_train_state
-            new_meta_params, new_meta_opt_state, meta_loss = outer_fn(meta_params, meta_opt_state, rng_key)
-            return (new_meta_params, new_meta_opt_state), meta_loss
+        # We sample a set of tasks.
+        task_env_params = sample_tasks(init_env_params, rng_key, num_tasks)
+        # We vmap the outer function optimisation loop across tasks.
+        batched_outer_fn = jax.vmap(outer_fn, in_axes=(0, 0, 0, 0), axis_name="tasks")
+
+        def iterate_fn(carry, rng_key):  # repeat many times to avoid going back to Python.
+            meta_params, meta_opt_state, task_env_params = carry
+            new_meta_params, new_meta_opt_state, meta_loss = batched_outer_fn(meta_params, meta_opt_state, jax.random.split(rng_key, num_tasks), task_env_params)
+            return (new_meta_params, new_meta_opt_state, task_env_params), meta_loss
         
-        (new_meta_params, new_meta_opt_state), meta_loss = jax.lax.scan(iterate_fn, (init_meta_params, init_meta_opt_state), jax.random.split(rng_key, iterations))
+        (new_meta_params, new_meta_opt_state, task_env_params), meta_loss = jax.lax.scan(iterate_fn, (init_meta_params, init_meta_opt_state, task_env_params), jax.random.split(rng_key, iterations))
         return new_meta_params, new_meta_opt_state, jnp.mean(meta_loss)
 
     return meta_learner_fn
@@ -188,66 +198,90 @@ def setup_experiment(seed, learning_rate):
 
     meta_params = network_fn.init(net_key, jnp.expand_dims(dummy_obs, 0))
 
-    opt_update_fn = optax.sgd(learning_rate, momentum=0.9)
+    opt_update_fn = optax.adam(learning_rate)
 
     meta_opt_state = opt_update_fn.init(meta_params)
 
     return meta_params, meta_opt_state, env, env_params, network_fn, opt_update_fn, key
 
-meta_params, meta_opt_state, env, init_env_params, network_fn, opt_update_fn, key = setup_experiment(seed=0, learning_rate=0.001)
+def get_eval_fn(env : Environment, network_fn):
+    """Returns a function that can be used to evaluate the agent on a single episode."""
 
+    @jax.jit
+    def eval_one_episode(params, rng, env_params):
+        """Evaluates the agent on a single episode."""
+        rng, reset_key = jax.random.split(rng)
+        o_tm1, state = env.reset(reset_key, env_params)
+        
+        def step(val):
+            params, state, o_tm1, tot_r, rng, _ = val
+            rng, key_step = jax.random.split(rng)
+            a_logits_t, _ = network_fn.apply(params, o_tm1[jnp.newaxis,])
+            a_t = distrax.Categorical(a_logits_t).sample(seed=rng)[0]
+            o_t, state, r_t, done, _  = env.step(key_step, state, a_t, env_params)
+            tot_r += r_t
+            return (params, state, o_t, tot_r, rng, done)
+        
+        params, state, o_t, tot_r, rng, done = jax.lax.while_loop(lambda val : val[5] == False, step, (params, state, o_tm1, 0, rng, False))
 
-@jax.jit
-def eval_one_episode(params, rng, env_params):
-    """Evaluates the agent on a single episode."""
-    rng, reset_key = jax.random.split(rng)
-    o_tm1, state = env.reset(reset_key, env_params)
+        return params, tot_r
     
-    def step(val):
-        params, state, o_tm1, tot_r, rng, _ = val
-        rng, key_step = jax.random.split(rng)
-        a_logits_t, _ = network_fn.apply(params, o_tm1[jnp.newaxis,])
-        a_t = distrax.Categorical(a_logits_t).sample(seed=rng)[0]
-        o_t, state, r_t, done, _  = env.step(key_step, state, a_t, env_params)
-        tot_r += r_t
-        return (params, state, o_t, tot_r, rng, done)
-       
-    params, state, o_t, tot_r, rng, done = jax.lax.while_loop(lambda val : val[5] == False, step, (params, state, o_tm1, 0, rng, False))
+    return jax.vmap(eval_one_episode, in_axes=(None, 0, None))
 
-    return params, tot_r
+def broadcast_to_shape(tree, shape):
+    """Broadcasts x to the given shape."""
+    return jax.tree_map(lambda x : jnp.broadcast_to(x, shape + x.shape), tree)
 
+def extract_from_first_device(x):
+    """Extracts the first element from the first device."""
+    return jax.tree_map(lambda x : x[0,0], x)
 
-key, eval_key = jax.random.split(key)
+if __name__ == "__main__":
 
-eval_keys = jax.random.split(eval_key, 100)
-tot_reward = jax.vmap(eval_one_episode, in_axes=(None, 0, None))(meta_params, eval_keys, init_env_params)[-1]
+    meta_params, meta_opt_state, env, init_env_params, network_fn, opt_update_fn, key = setup_experiment(seed=0, learning_rate=0.001)
 
-print("Before Training Reward: ", jnp.mean(tot_reward))
+    eval_fn = get_eval_fn(env, network_fn)
+   
 
-meta_learn_fn = get_meta_learner_fn(env,
-                                    init_env_params, 
-                                    network_fn, 
-                                    opt_update_fn, 
-                                    rollout_len=64, 
-                                    agent_discount=0.99, 
-                                    iterations=100, 
-                                    gae_lambda=0.95, 
-                                    clip_epsilon=0.2,
-                                    num_trajectories=64,
-                                    inner_loop_burn_in=1,
-                                    num_tasks=1)
+    key, eval_key = jax.random.split(key)
 
-meta_learn_fn = jax.jit(meta_learn_fn)
+    eval_keys = jax.random.split(eval_key, 100)
+    tot_reward = eval_fn(meta_params, eval_keys, init_env_params)[-1]
 
-meta_training_steps = 5
-print("Meta-learning...")
+    print("Before Training Reward: ", jnp.mean(tot_reward))
+    num_tasks = 10
+    meta_learn_fn = get_meta_learner_fn(env,
+                                        init_env_params, 
+                                        network_fn, 
+                                        opt_update_fn, 
+                                        rollout_len=500, 
+                                        agent_discount=0.99, 
+                                        iterations=10, 
+                                        gae_lambda=0.95, 
+                                        clip_epsilon=0.1,
+                                        num_trajectories=64,
+                                        inner_loop_burn_in=1,
+                                        num_tasks=num_tasks)
 
-for i in range(meta_training_steps):
-    meta_params, meta_opt_state, meta_loss = meta_learn_fn(meta_params, meta_opt_state, key)
-    print("Meta Loss: ", meta_loss)
+    meta_learn_fn = jax.pmap(meta_learn_fn, axis_name="devices")
 
-key, eval_key = jax.random.split(key)
-eval_keys = jax.random.split(eval_key, 100)
-tot_reward = jax.vmap(eval_one_episode, in_axes=(None, 0, None))(meta_params, eval_keys, init_env_params)[-1]
+    meta_training_steps = 50
+    print("Meta-learning...")
 
-print("After Training Reward: ", jnp.mean(tot_reward))
+    key, train_key = jax.random.split(key)
+    train_keys = jax.random.split(train_key, jax.local_device_count())
+    reshape = lambda x: x.reshape((jax.local_device_count(), ) + x.shape[1:])
+    train_keys = reshape(jnp.stack(train_keys)) 
+
+    meta_params, meta_opt_state = broadcast_to_shape((meta_params, meta_opt_state), (jax.local_device_count(),num_tasks))
+
+    for i in range(meta_training_steps):
+        meta_params, meta_opt_state, meta_loss = meta_learn_fn(meta_params, meta_opt_state, train_keys)
+        print("Meta Loss: ", jnp.mean(meta_loss))
+
+    meta_params = extract_from_first_device(meta_params)
+    key, eval_key = jax.random.split(key)
+    eval_keys = jax.random.split(eval_key, 100)
+    tot_reward = eval_fn(meta_params, eval_keys, init_env_params)[-1]
+
+    print("After Training Reward: ", jnp.mean(tot_reward))
